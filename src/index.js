4e68b0842d5cd3938015bda099accef246124de2
paulzay <paulotieno2@gmail.com>
2020-05-20 21:25:27 +0300

    build app entry point

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { logger } from 'redux-logger';
+import reducer from './reducers';
+import './assets/css/app.css';
+import App from "./components/App";
+
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger)
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
