92988a23ad9b1e956624f7459ac8ed320e64e93d
paulzay <paulotieno2@gmail.com>
2020-05-21 18:15:38 +0300

    add button component

---
+++ src/containers/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../redux/actions/index';
+
+let Button = ({ getPosts, channel }) => (
+  <button
+    onClick={() => { getPosts(channel) }}
+    className="btn btn-primary btn-lg btn-block" >
+    Get top news
+  </button>
+);
+
+const mapStateToProps = (state) => ({
+  channel: state.channel
+})
+
+const mapDispatchToProps = {
+  getPosts: fetchPosts
+}
+
+Button = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Button)
+
+export default Button;
