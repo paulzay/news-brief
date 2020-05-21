eea2652373674dfefa534082284ee892b930c4af
paulzay <paulotieno2@gmail.com>
2020-05-21 18:14:25 +0300

    add channelsfield component

---
+++ src/components/ChannelsField.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Channel from '../containers/Channel'
+
+const ChannelsField = () => (
+
+  <div className="row" >
+    <Channel channelName="BBC" channelString="bbc-news" />
+    <Channel channelName="CNBC" channelString="cnbc" />
+    <Channel channelName="CNN" channelString="cnn" />
+    <Channel channelName="FT" channelString="financial-times" />
+    <Channel channelName="ESPN" channelString="espn" />
+    <Channel channelName="TIME" channelString="time" />
+  </div>
+);
+
+export default ChannelsField;
