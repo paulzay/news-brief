7b461c89c2ec838b249308c43b9f628de30e2357
paulzay <paulotieno2@gmail.com>
2020-05-21 18:12:39 +0300

    add the topnews component

---
+++ src/containers/TopNews.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import  NewsItem  from '../components/NewsItem';
+
+let TopNews = ({ channels, loading }) => {
+    let topNews = "";
+
+    if (channels) {
+        topNews = channels.map((article, index) =>
+            (
+                <div key={`${index}`} className="row">
+                    <NewsItem article={channels[index]} />
+                </div>
+            )
+        )
+    }
+    if (loading) {
+        topNews = <h3 className="loading-indicator">Loading ...</h3>
+    }
+
+    return (
+        <div>
+            {topNews}
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    channels: state.json,
+    loading: state.loading
+})
+
+TopNews = connect(
+    mapStateToProps,
+    null
+)(TopNews)
+
+export default TopNews;
